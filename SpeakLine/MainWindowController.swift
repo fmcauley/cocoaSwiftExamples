7e30a9881f5a5e74e4770f53c9d33c1d97d1a2a5
Frank McAuley <fmcauley@taivara.com>
2015-05-26 09:53:45 -0400

    made updates that add the speech synch to the project

--- SpeakLine/MainWindowController.swift
+++ SpeakLine/MainWindowController.swift
@@ -14,6 +14,13 @@ class MainWindowController: NSWindowController {
     @IBOutlet weak var speakButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     
+    let speechSynth = NSSpeechSynthesizer()
+    var isStarted: Bool = false {
+        didSet{
+            updateButtons()
+        }
+    }
+    
     
     override var windowNibName: String {
         return "MainWindowController"
@@ -21,8 +28,20 @@ class MainWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
+        updateButtons()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    // MARK: - Private Methods
+    func updateButtons(){
+        if isStarted{
+            speakButton.enabled = false
+            stopButton.enabled = true
+        } else {
+            stopButton.enabled = false
+            speakButton.enabled = true
+        }
     }
     
     // MARK: - Action Methods
@@ -32,12 +51,14 @@ class MainWindowController: NSWindowController {
         if string.isEmpty{
             println("string from \(textField) is empty")
         } else {
-            println("stirng is \"\(textField.stringValue)\"")
+            speechSynth.startSpeakingString(string)
+            isStarted = true
         }
     }
     
     @IBAction func stopIt(sender: NSButton){
-        println("stop button clicked")
+        speechSynth.stopSpeaking()
+        isStarted = false
     }
     
 }
